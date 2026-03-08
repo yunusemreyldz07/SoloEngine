b0755dd875c0b69a08df1d56feb83786b0c57552
solod3v <yunusemreyldz2007@gmail.com>
2026-03-09 00:55:07 +0300

    Refactor evaluation functions: update evaluate_board signature to include optional parameter for piece-square table evaluation
    bench: 327292

--- evaluation.h
+++ evaluation.h
@@ -3,7 +3,10 @@
 
 #include "board.h"
 // Evaluation functions
-int evaluate_board(const Board& board, bool include_pawn_penalties = true);
+int evaluate_board(const Board& board, bool include_pawn_penalties = true, bool include_psqt = true);
 void evaluate_pawn_penalties(const Board& board, int mg[2], int eg[2]);
-
+extern const int mg_value[6];
+extern const int eg_value[6];
+extern const int* mg_pesto_tables[6];
+extern const int* eg_pesto_tables[6];
 #endif
