4b8d5e5482080670e94897f4dab77c8fead45771
solod3v <yunusemreyldz2007@gmail.com>
2026-03-18 21:45:33 +0300

    bench: 273882

--- search.cpp
+++ search.cpp
@@ -421,7 +421,7 @@ int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, S
         std::vector<Move> childPv; 
         // Singular Extensions
         int extension = 0;
-        bool trySingular = !(ply == 0) && !ss->singularMove && ttHit && (chosenMove == ttMove) && depth >= 8 && (ttEntry.flag != TT_BETA) && (ttEntry.depth >= depth - 3);
+        bool trySingular = !(ply == 0) && !ss->singularMove && ttHit && (chosenMove == ttMove) && depth >= 6 && (ttEntry.flag != TT_BETA) && (ttEntry.depth >= depth - 3);
         if (trySingular) {
             int16_t ttSeScore = ttEntry.score;
             if (ttSeScore >= MATE_SCORE - MAX_PLY) {
