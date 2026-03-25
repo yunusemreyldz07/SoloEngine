9648f5685b094faa3b20d988d368729dd2e0c300
solod3v <yunusemreyldz2007@gmail.com>
2026-03-25 21:15:04 +0300

    lower Internal Iterative Reductions (IIR) depth threshold from 5 to 4
    bench: 212659

--- search.cpp
+++ search.cpp
@@ -356,9 +356,11 @@ int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, S
         }
     }
 
-    if ((!ttHit || ttMove == 0 || ttEntry.depth < depth - 3) && depth >= 5) {
+    // Internal Iterative Reductions (IIR)
+    if ((!ttHit || ttMove == 0 || ttEntry.depth < depth - 3) && depth >= 4) {
         depth--;
     }
+
 
 
     int moveCount = 0;
