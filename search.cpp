dd42ed432d32e09e977c2c93ccd3f3b5f42146ff
Yunus Emre Yıldız <yunusemreyldz2007@gmail.com>
2026-04-05 20:22:30 +0300

    Razoring was added. (#105)
    * Added razoring.
    
    Bench: 148211
    
    * Fixed wrong razoring code.
    
    Bench: 177918

--- search.cpp
+++ search.cpp
@@ -393,6 +393,15 @@ int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, S
         }
     }
 
+    // Razoring. if eval is far below alpha even the best capture, there's no point in searching. drop straight into qsearch.
+    if (!pvNode && !rootNode && depth <= 3 && staticEval + 300 + 60 * depth < alpha){
+        int16_t razoringScore = qsearch(board, alpha, beta, ply, ss);
+
+        if (razoringScore < alpha) {
+            return razoringScore;
+        }
+    }
+
     // Null Move Pruning
     if (!rootNode && !ss->singularMove && !inCheck && depth >= 3 && !pvNode) {
         const int prevEnPassant = board.enPassant;
