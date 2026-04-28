9d84a627cd73ca13cfff656f1ef732b3106a2d95
solod3v <yunusemreyldz2007@gmail.com>
2026-04-28 22:33:52 +0300

    Replaced "dynamic memory" pv tables with "static" ones.
    Bench: 207134

--- search.h
+++ search.h
@@ -14,7 +14,7 @@ struct SearchStack {
     int16_t staticEval;
 };
 
-int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, SearchStack* ss, std::vector<Move>& pvLine, std::vector<uint64_t>& positionHistory);
+int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, SearchStack* ss, Move pvTable[][MAX_PLY], int pvLength[], std::vector<uint64_t>& positionHistory);
 
 Move getBestMove(Board& board, int maxDepth, int movetimeMs, const std::vector<uint64_t>& positionHistory, int ply, bool silent, int16_t& outScore);
 
